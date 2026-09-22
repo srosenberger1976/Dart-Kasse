dfbbc9cbe77f4e0287038d0589050c4a596fe7ff
srosenberger1976 <s.rosenberger1976@gmail.com>
2026-09-22 12:11:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'app-cache-v1';
+const CACHE_NAME = 'app-cache-v2';
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', (event) => {
 event.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
